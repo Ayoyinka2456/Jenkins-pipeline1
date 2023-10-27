25149acf15ba0461dd534fb72335b4ec2e1ee154
Ayoyinka2456 <108306542+Ayoyinka2456@users.noreply.github.com>
2023-10-27 00:25:56 -0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,54 +1,58 @@
 pipeline {
     agent {
-        label 'node1'
-    }
+        label
+    } 'node1'
     tools {
         maven 'Maven'
         jdk 'Java11'
     }
     stages {
+
         stage('Checkout') {
             steps {
                 git 'https://github.com/Ayoyinka2456/Jenkins-pipeline1.git'
             }
         }
-
         stage('Build') {
             steps {
                 sh 'mvn clean install'
             }
         }
-
         stage('Test') {
+
             steps {
                 sh 'mvn test'
                 stash(name: 'packaged_code', includes: 'target/*.war')
             }
         }
-
         stage('Deploy to Tomcat') {
             agent {
                 label 'node2_tomcat'
             }
             steps {
                 unstash 'packaged_code'
+                sh "sudo rm -rf ~/apache*/webapp/*.war"
                 sh "sudo mv target/*.war ~/apache*/webapps/"
                 sh "sudo ~/apache*/bin/shutdown.sh && sudo ~/apache*/bin/startup.sh"
             }
         }
     }
-         post{
-            // success {
-            //     emailext body: 'Your Jenkins pipeline was successful', subject: 'BUILD SUCCESS'
-            // }
-            // failure {
-            //     emailext body: 'Your Jenkins pipeline failed', subject: 'BUILD FAILED', to: 'kolawoleayoyinka.ka@gmail.com'
-            // }
-            // emailext body: '''$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS:
-                always {
+        post{
+            always {
+                script {
+                    def frowny = '😞'
+                    def smiley = '😄'
+                    def what_reaction = null
+                    if [[ $BUILD_STATUS == "Successful" ]]; then
+                        what_reaction=$smiley
+                    else
+                        what_reaction=$frowny
+                    fi
+
                     emailext body: 'Check console output at $BUILD_URL to view the results.', 
-                    subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!', 
+                    subject: '$what_reaction $PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!', 
                     to: 'eas.adeyemi@gmail.com'
                 }
+            }
         }     
     }
